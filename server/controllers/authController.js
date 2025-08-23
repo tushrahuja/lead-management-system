05c390f35678cca26e1e2dd57fa2d403b2a593e9
tushrahuja <ahuja.tushar.m@gmail.com>
2025-08-23 21:11:14 +0530

    cookie setting changed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -71,7 +71,9 @@ const loginUser = async (req, res) => {
 const logoutUser = (req, res) => {
     res.cookie('token', '', {
         httpOnly: true,
-        expires: new Date(0)
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
+        expires: new Date(0) 
     });
 
     res.status(200).json({ message: 'Logged out successfully' });
